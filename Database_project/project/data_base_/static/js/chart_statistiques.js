d59f29a23e9d82324e519909fbf918ce7f0a4241
Ebot fabien <touchone0001@gmail.com>
2021-12-06 08:51:14 +0100

    done

--- Database_project/project/data_base_/static/js/chart_statistiques.js
+++ Database_project/project/data_base_/static/js/chart_statistiques.js
@@ -1,3 +1,4 @@
+
 async function getMissionDataPerYearData () {
     const URL = 'https://amexpert10.ddns.net/dashboard/missionperyear'
     const response = await fetch(URL)
@@ -64,6 +65,50 @@ getMissionsPerMonth().then(amountPerMonth => {
     missionsPerMonth.update()
 })
 
+async function getExpertTotalPerMonth () {
+    const URL = 'https://amexpert10.ddns.net/dashboard/expertencashpermonth'
+    const response = await fetch(URL)
+    const datapoints = await response.json()
+    return datapoints
+}
+
+getExpertTotalPerMonth().then(amountPerMonth => {
+    const months = amountPerMonth.data.map(
+        function (index) {
+            return index.month
+        })
+    const total = amountPerMonth.data.map(
+        function (index) {
+            return index.total
+        })
+    expertTotalPerMonth.config.data.labels = months
+    expertTotalPerMonth.config.data.datasets[0].data  = total
+    expertTotalPerMonth.config.data.datasets[0].label = 'Chiffre d\'affaire (€) par mois sur l\'année en cours'
+    expertTotalPerMonth.update()
+})
+
+
+async function getExpertTotalPerYear () {
+    const URL = 'https://amexpert10.ddns.net/dashboard/expertencashperyear'
+    const response = await fetch(URL)
+    const datapoints = await response.json()
+    return datapoints
+}
+
+getExpertTotalPerYear().then(amountPerMonth => {
+    const year = amountPerMonth.data.map(
+        function (index) {
+            return index.year
+        })
+    const total = amountPerMonth.data.map(
+        function (index) {
+            return index.total
+        })
+    expertTotalPerMonth.config.data.labels = year
+    expertTotalPerMonth.config.data.datasets[0].data  = total
+    expertTotalPerMonth.config.data.datasets[0].label = 'Chiffre d\'affaire (€) par An'
+    expertTotalPerMonth.update()
+})
 // setup 
 const data = {
     type:'',
@@ -125,6 +170,17 @@ const missionsPerMonth = new Chart(
 
 const totalAmountPerYear = new Chart(
     document.getElementById('totalAmountPerYear'),
+    config
+);
+
+const expertTotalPerMonth = new Chart(
+    document.getElementById('expertTotalPerMonth'),
+    config
+);
+
+
+const expertTotalPerYear = new Chart(
+    document.getElementById('expertTotalPerYear'),
     config
 );
 
